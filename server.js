342e91daff7fcccb52e772547bb2faced2c85c54
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-27 22:31:26 -0600

    Add the get methods.

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const pg = require('pg')
-
 const app = express()
 
 const environment = process.env.NODE_ENV || 'development'
@@ -17,6 +15,23 @@ app.get('/', (req, res)=>{
     res.send('Hello! Universe')
 })
 
+app.get('/api/v1/papers', async (req, res)=>{
+    try{
+        const papers = await database('papers').select()
+        res.status(200).json(papers)
+    }catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/v1/footnotes', async (req, res)=>{
+    try{
+        const footnotes = await database('footnotes').select()
+        res.status(200).json(footnotes)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
 
 app.listen(PORT, ()=>{
     console.log(`${app.locals.title} is running on port ${PORT}`)
