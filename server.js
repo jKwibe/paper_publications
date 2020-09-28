f71702deea57f7e77819c880ef9393b7e14852ab
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-27 22:17:35 -0600

    Import the database to the server

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const pg = require('pg')
-const knex = require('knex')
 
 const app = express()
 
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
+app.locals.title = 'Publications'
+
+
+
 const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res)=>{
+    res.send('Hello! Universe')
+})
 
 
 app.listen(PORT, ()=>{
-    console.log(`Your app is running on port ${PORT}`)
+    console.log(`${app.locals.title} is running on port ${PORT}`)
 })
