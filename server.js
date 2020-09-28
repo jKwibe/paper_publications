e4b37a82f39953c67168a8eb59ad423ffb58f6bb
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-28 11:04:16 -0600

    Add get footnotes route.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
@@ -30,6 +32,72 @@ app.get('/api/v1/footnotes', async (req, res)=>{
         res.status(200).json(footnotes)
     }catch (error){
         res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/api/v1/papers', async (req, res)=>{
+    const paper = req.body;
+    console.log(paper)
+    for(let requiredParam of ['title', 'author']){
+        if (!paper[requiredParam]) {
+            return res.status(500).json({message: `You are missing ${requiredParam}`})
+        }
+    }
+
+    try{
+       const newPaper = await database('papers').insert(paper, '*')
+        res.status(201).json(newPaper)
+    }catch (error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/api/v1/papers/:id/footnote', async (req, res)=>{
+    const paper = await database('papers').whereRaw('id = ?', [req.params['id']])
+
+    if (!paper[0]){
+        return res.status(404).json({message: `Resource not found`})
+    }
+
+    const footnote = await database('footnotes').insert({note: req.body['note'], paper_id: req.params['id']}, '*')
+
+    try{
+       res.status(200).json(footnote)
+    }catch (error){
+        res.status(500).json({message: 'There was an error saving the footnote'})
+    }
+})
+
+app.get('/api/v1/papers/:id', async (req, res)=>{
+    console.log(req.params.id)
+    const paper = await  database('papers')
+                        .where('id', req.params.id)
+                        .select('id', 'author', 'publisher')
+
+    if (!paper[0]){
+       return res.status(404).json({ message: 'resource not found'})
+    }
+    try{
+        res.status(200).json(paper[0])
+    }catch (error) {
+        res.status(500).json({message: 'server error'})
+    }
+
+})
+
+app.get('/api/v1/papers/:id/footnotes', async (req, res)=>{
+    const paperId = req.params.id
+    const paper = await database('papers').where('id', paperId)
+
+    if (!paper[0]){
+        return res.status(404).json({message: 'Resource not found'})
+    }
+    const footnotes = await database('footnotes').where('paper_id', paperId).select('id', 'note')
+
+    try{
+        res.status(200).json(footnotes)
+    }catch (error) {
+        res.status(500).json({message: 'server error'})
     }
 })
 
